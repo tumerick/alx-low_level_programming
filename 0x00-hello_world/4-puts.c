a5b3fa5060a18b576590281941b70a1c49531456
tumerick <magukwert@gmail.com>
2022-09-08 23:24:23 +0200

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /*
  *	main :: entry point
- *	
- * 	return :: int
+ *	description :: returns a string
+ *	return :: int
  */
 
-int main( void )
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
