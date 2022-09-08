4fa23ac07d045ce9ef45e24f6da9dfcebe68c9e9
tumerick <magukwert@gmail.com>
2022-09-08 23:32:16 +0200

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /*
- *	main - entry point
- *	Description - prints "Programming is like building a multilingual puzzle"
- *
- *	Return: 0 (Success)
+ * main - entry point
+ * 
+ * Return: 0 (Success)
  */
 
 int main(void)
