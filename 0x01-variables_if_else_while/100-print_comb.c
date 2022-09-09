be976717b8c43fed9613c338e6acfff631bc75f0
tumerick <magukwert@gmail.com>
2022-09-09 12:49:52 +0200

    commit

---
+++ 0x01-variables_if_else_while/100-print_comb.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry line.
+ *
+ * Description: prints all combinations of single-digit numbers.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 48;
+	int m = 48;
+
+	for (; n < 58; n++)
+	{
+		m = n + 1;
+
+		for (; m < 58; m++ )
+		{
+			putchar(n);
+			putchar(m);
+
+			if (n < 56 || m < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+
+	putchar(10);
+
+	return (0);
+}
