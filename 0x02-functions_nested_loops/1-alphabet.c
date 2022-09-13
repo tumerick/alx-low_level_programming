e5774f1a38aeffbe4401224400090e562b09ce5a
tumerick <magukwert@gmail.com>
2022-09-13 16:11:57 +0200

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry line
+ *
+ * Description: prints the letters of the alphabet
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar(10);
+
+	return (0);
+}
