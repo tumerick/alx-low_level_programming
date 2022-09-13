551037f846ff06edb9944a700c5eb9a84670ab9c
tumerick <magukwert@gmail.com>
2022-09-13 17:13:07 +0200

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of number
+ *
+ * @n: type int
+ *
+ * Description: Function recieves @n and prints '+','0','-' if
+ * the integer is positive, zero, or negative - respectively.
+ *
+ * Return: 1 (Value < 0); 0 (Value == 0); -1 (Value < 0)
+ */
+
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+
+	if (n == 0)
+	{
+		_putchar(48);
+		return (n);
+	}
+}
