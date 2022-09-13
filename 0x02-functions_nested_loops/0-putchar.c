5db4a7cd4797c454a2d1e6d4433abf87c20fa3f0
tumerick <magukwert@gmail.com>
2022-09-13 14:01:23 +0200

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry line
+ *
+ * Description: prints "_putchar\n" the the console.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("__putchar\n");
+
+	return (0);
+}
