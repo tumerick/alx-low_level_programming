d631a404fa42d7a00be753ef4f2ea196ea114afe
tumerick <magukwert@gmail.com>
2022-09-13 17:00:47 +0200

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphaneumeric
+ * @c: type int
+ *
+ * Description: checks if input is an alphaneumeric character
+ *
+ * Return: 1 (Success); 0 (Not Successful)
+ */
+
+int _isalpha(int c);
+{
+	if (c >= 'A' && <= 'z')
+	{
+		return (1);
+	}
+
+	/* falsy */
+	return (0);
+}
