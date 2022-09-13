db3b527e2837d3253d3e13bda10c38ccec0b2b29
tumerick <magukwert@gmail.com>
2022-09-13 17:20:29 +0200

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - returns absolute value
+ *
+ * @int: type int
+ *
+ * Description: calculates the absolute value of a given value.
+ *
+ * Return: type int.
+ */
+
+int _abs(int)
+{
+	return abs(num);
+}
