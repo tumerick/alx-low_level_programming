6c6c9c2ee3072039e51a40270b1694c19e9706a6
tumerick <magukwert@gmail.com>
2022-09-14 12:49:00 +0200

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,8 +2,8 @@
 
 /**
  * print_times_table - prints the n times table.
- * 
- * @n: 
+ *
+ * @n: <int>
  *
  * Return: void
  */
@@ -14,39 +14,32 @@ void print_times_table(int n)
 
 	if (n >= 15 || n <= 0)
 	{
-		n = 0;
+		return;
 	}
-
 	for (multiplier = 0; multiplier <= n; multiplier++)
 	{
 		for (multiplicand = 0; multiplicand <= n; multiplicand++)
 		{
 			product = multiplicand * multiplier;
 
-			if ( multiplicand == 0)
+			if (multiplicand == 0)
 			{
 				_putchar(product + '0');
-			}
-
-			else if (product < 10 && multiplicand != 0)
+			} else if (product < 10 && multiplicand != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(product + '0');
-			}
-
-			else if (product >= 10 && multiplicand < 100)
+			} else if (product >= 10 && product < 100)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
-			}
-
-			else if (product >- 100)
+			} else if (product >= 100)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -57,5 +50,4 @@ void print_times_table(int n)
 		}
 		_putchar(10);
 	}
-
 }
