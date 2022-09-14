d71a8a0073ba5cb31d68e8bdae21d211dfa910d4
tumerick <magukwert@gmail.com>
2022-09-14 14:22:20 +0200

    commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry line.
+ *
+ * Description: prints even numbers of the fibonacci sequence
+ * not exceeding 4,000,000.
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i = 1, j = 1, res = 0;
+	int k;
+
+	while (j <= 4000000)
+	{
+		if (j % 2 != 0)
+		{
+			continue
+		}
+
+		res += j;
+
+		k = j;
+		j += i;
+		i = k;
+	}
+
+	printf("%d\n", res);
+
+	return (0);
+}
