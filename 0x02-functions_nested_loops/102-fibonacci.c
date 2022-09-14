64362684abcf5238faf81f51f14ab06caabf0faf
tumerick <magukwert@gmail.com>
2022-09-14 13:50:44 +0200

    commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,39 +2,6 @@
 #include <stdlib.h>
 
 /**
- * fibonacci - calculates the fibonacci sequence recursively.
- * (with memoization).
- *
- * @n: <int>
- * @cache: <int> array used for memoization.
- *
- * Return: fibonacci(n - 1) + fibonacci( n + 2 )
- */
-
-int fibonacci(int *cache, int n)
-{
-	int result;
-
-	if (cache[n] != -1)
-	{
-		return (cache[n]);
-	}
-
-	if (n < 2)
-	{
-		result = n;
-	}
-
-	else
-	{
-		result = (fibonacci(cache, n - 1) + fibonacci(cache, n - 2));
-	}
-
-	cache[n] = result;
-	return (result);
-}
-
-/**
  * main - Entry line.
  *
  * Description: prints the fibonacci sequence.
@@ -44,32 +11,22 @@ int fibonacci(int *cache, int n)
 
 int main(void)
 {
-	int i;
-	int n = 50;
-	int cache[50];
-
-	for (i = 0; i < n; i++)
+	int c = 2, i = 0;
+	long int i = 1, j = 2, k;
+	
+	for (; i <= 50; i++)
 	{
-		cache[i] = -1;
-	}
-
-	cache[0] = 0;
-	cache[1] = 1;
-
-	fibonacci(cache, n - 1);
-
-	for (i = 2; i <= n; i++)
-	{
-		if (i == 50)
+		if (c == 50)
 		{
-			printf("%d\n", cache[i]);
+			printf("%lu\n", j);
+			break;
 		}
 
-		else
-		{
-			printf("%d, ", cache[i]);
-		}
-	}
+		printf("%lu, ", j);
 
+		k = j;
+		j += i;
+		i = l;
+	}
 	return (0);
 }
