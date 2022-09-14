e2cab77427ca17ecff66e475b1396d3260127d9a
tumerick <magukwert@gmail.com>
2022-09-14 11:35:16 +0200

    commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int c);
 int _abs(int n);
 int print_last_digit(int n);
-void jack_bauer(void);
-void times_table(void);
+int add(int a, int b);
 #endif
