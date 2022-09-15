5f970e6a821f69fbd2950dec61d873177bcd9c46
tumerick <magukwert@gmail.com>
2022-09-15 19:58:13 +0200

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,4 @@
-#include "main"
+#include "main.h"
 
 /**
  * print_numbers - prints the numbers 1 - 9
@@ -9,9 +9,10 @@ void print_numbers(void)
 {
 	int i = 48;
 
-	while (i < 57)
+	while (i <= 57)
 	{
 		_putchar(i);
+		i++;
 	}
 	_putchar(10);
 }
