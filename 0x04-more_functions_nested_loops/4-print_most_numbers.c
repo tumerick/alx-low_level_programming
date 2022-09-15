5bf32f4563aff97e3f3d18f5b9102615c59c9176
tumerick <magukwert@gmail.com>
2022-09-15 20:05:31 +0200

    commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 1 - 9 excluding 2 || 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+
+		_putchar(i + 48);
+	}
+}
