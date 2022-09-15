391695614ede3f43d3222a45947830ec645ff7b2
tumerick <magukwert@gmail.com>
2022-09-15 13:26:37 +0200

    commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 
 void positive_or_negative(int i);
 
-int largest_numer(int a, int b, int c);
+int largest_number(int a, int b, int c);
 
 #endif
