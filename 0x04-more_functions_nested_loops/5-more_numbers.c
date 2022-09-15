325c45c2e38dc0b806b2a66e364725d3b7f6e132
tumerick <magukwert@gmail.com>
2022-09-15 20:21:12 +0200

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,18 +8,16 @@
 
 void more_numbers(void)
 {
-	int i = 10;
-	int j = 0;
-	int c;
+	int i, j, c;
 
-	for (; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			c = j
+			c = j;
 			if (j > 9)
 			{
-				_putchar(1 + 49);
+				_putchar(1 + 48);
 				c = j % 10;
 			}
 			_putchar(c + 48);
