97388e1435b739361b527627f6fb28beb8254c26
tumerick <magukwert@gmail.com>
2022-09-15 11:07:35 +0200

    commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Tests whether positive_or_negative() function runs correctly.
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(0);
+
+	return (0);
+}
