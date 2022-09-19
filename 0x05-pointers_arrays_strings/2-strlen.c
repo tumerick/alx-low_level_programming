7d23b8ea780ae5f51d94dc5aeeb5ef2149877f1e
tumerick <magukwert@gmail.com>
2022-09-19 13:47:50 +0200

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: <char>
+ *
+ * Return: <int> (Success);
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return len;
+}
