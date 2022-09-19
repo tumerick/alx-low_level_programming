5551cc15d1333e52c6f526a27750461c64473eed
tumerick <magukwert@gmail.com>
2022-09-19 22:21:17 +0200

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ *
+ * @str: <char> array.
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int full_length = _strlen(str) - 1;
+	int half_length = full_length / 2;
+
+	while (half_length <= full_length)
+	{
+		_putchar(str[half_length];
+	}
+	_putchar(10);
+}
