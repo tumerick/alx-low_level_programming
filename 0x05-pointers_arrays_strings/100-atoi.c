7a1ae45e035acda75f2550a9dd6b019900152991
tumerick <magukwert@gmail.com>
2022-09-20 00:02:04 +0200

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,5 @@
 #include "main.h"
-#include <limits.h>
-
+#include <stdio.h>
 /**
  * _atoi - converts a string to an integer
  *
@@ -11,31 +10,6 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, base = 0, i = 0;
+	printf("%d", *str < '0');
 
-	while (s[i] == ' ')
-	{
-		i++;
-	}
-
-	if (s[i] == '-' || s[i] == '+')
-	{
-		sign = 1 - 2 * (s[i++] == '-');
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (base > INT_MAX / 10
-		|| (base == INT_MAX / 10
-		&& s[i] - '0' > 7))
-		{
-			if (sign == 1)
-				return INT_MAX;
-			else
-				return INT_MIN;
-		}
-
-		base = 10 * base + (s[i++] - '0');
-	}
-	return (base * sign);
 }
