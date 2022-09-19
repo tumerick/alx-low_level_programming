1f86100ba6bdae1a81a813d4e997ccaf59915b49
tumerick <magukwert@gmail.com>
2022-09-19 21:10:45 +0200

    commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -10,12 +10,14 @@
 
 int _strlen(char *s)
 {
-	int len = 0;
+	/* 
+	 * create a pointer *s
+	 * *t == t[0]
+	 */
 
-	while (*s != '\0')
-	{
-		s++;
-		len++;
-	}
-	return (len);
+	char *t = s;
+
+	while (*t) ++p;
+
+	return (p - s);
 }
