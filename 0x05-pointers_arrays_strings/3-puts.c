450af50a4be5430a6dedff6814ccfc69a5acfc50
tumerick <magukwert@gmail.com>
2022-09-19 16:39:30 +0200

    commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - prints <char> str followed by '\n'
+ *
+ * @str: <char> string value to print.
  *
  * Return: void
  */
@@ -13,12 +16,12 @@ void _puts(char *str)
 	while (str[i])
 	{
 		if (putchar(str[i]) == EOF)
-			return EOF;
+			return;
 		i++;
 	}
 
 	if (putchar('\n') == EOF)
-		return EOF;
+		return;
 
 	return;
 }
