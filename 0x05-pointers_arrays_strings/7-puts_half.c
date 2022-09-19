4d9113c2a3f5dafce070ab5bb9ce6f614c7c87da
tumerick <magukwert@gmail.com>
2022-09-19 22:52:55 +0200

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,18 +13,14 @@ void puts_half(char *str)
 {
 	int f_len = 0, h_len;
 
-	while (*str)
+	while (str[f_len] != '\0')
 		f_len++;
-	f_len--;
 
-	if (f_len % 2 == 0)
-		h_len = f_len / 2;
-	else
-		h_len = (f_len - 1) / 2;
+	h_len = f_len / 2;
 
 	while (h_len < f_len)
 	{
-		_putchar(str[++h_len]);
+		_putchar(str[h_len++]);
 	}
 	_putchar(10);
 }
