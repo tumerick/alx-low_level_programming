cb67b85577ae807b997b75ac270ca19fc8764e37
tumerick <magukwert@gmail.com>
2022-09-21 14:28:48 +0200

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - appends the `src` string to the `dest string.
+ *
+ * @src: `src` <char> array
+ * @dest: `dest` <char> array
+ *
+ * @Return: concatented <char> array
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *res = dest;
+
+	while (*dest)
+		dest++;
+	
+	while (*dest++ = *src++)
+		;
+	return (res);
+}
