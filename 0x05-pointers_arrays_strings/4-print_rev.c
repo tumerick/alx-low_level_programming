7ad49f117fdb0f6f367fd3539dd5ffcac8801383
tumerick <magukwert@gmail.com>
2022-09-21 14:14:51 +0200

    commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,22 +12,13 @@
 void print_rev(char *s)
 {
 	char *t = s;
-	
-	/*
-	 * if s == 'Hello'
-	 * *t = s == s[0] == 72;
-	 * t++ == s[1] == 101;
-	 * t++ == s[2] == 108;
-	 * and so on...
-	 *
-	 *
-	 */
+
 	while (*t)
 		t++;
 
 	t--;
 
-	while (*t > *s)
+	while (*t)
 	{
 		_putchar(*t);
 		t--;
