a037f56efbe60f44a3c512bc44a3a4cb1aabefce
tumerick <magukwert@gmail.com>
2022-09-21 14:08:34 +0200

    commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,46 +12,22 @@
 
 void rev_string(char *s)
 {
-	/* 
-	 * create an intermediate 
-	 * pointer to s: *end
-	 * 
-	 * -- based on this: (incoming link)
-	 * https://stackoverflow.com/questions/72630711/iterate-over-str-vs-str-in-while-loop-in-c
-	 * 
-	 * *end == end[0]
-	 */
 	char *end = s;
 	char tmp = 0;
 
-	/* base case */
 	if (!s)
 		return;
-	
-	/* 
-	 * Get the pointer to the end
-	 * of the string
-	 */
+
 	while (*end)
 	{
-		/* s[0], s[1]... s[i++] */
 		end++;
 	}
-	/* not the "\0" */
 	end--;
 
-	/*
-	 * at this point 
-	 * end == s[-1]
-	 */
 	while (end > s)
 	{
-		/* swap */
 		tmp = *end;
-		/* some */
 		*end-- = *s;
-		/* stuff */
 		*s++ = tmp;
-		/* around */
 	}
 }
