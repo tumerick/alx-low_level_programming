55660ced2b431dc44fa5d4c779b693730d5844cf
tumerick <magukwert@gmail.com>
2022-09-22 16:43:48 +0200

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,16 +2,19 @@
 
 /**
  * _strcmp - compares two strings
- * s1: <char> array
- * s2: <char> array
+ * @s1: <char> array
+ * @s2: <char> array
  *
  * Return: <int>
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1++ == *s2++))
-		;
-	return *(const unsigned char*)s1 - *(const unsigned char*)s2;
-		
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++
+	};
+
+	return (*(const unsigned char *) s1 - *(const unsigned char *) s2);
 }
