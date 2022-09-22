7d465af3124276bac0a90a22be1895267c1dbf73
tumerick <magukwert@gmail.com>
2022-09-22 15:11:29 +0200

    commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,28 @@
+#include "main.h"
 
+/**
+ * _strncat - similar to _strcat except that it
+ * uses at most `n` bytes from `src` and `src` does
+ * not need to be null-terminated if it contains `n`
+ * more bytes.
+ *
+ * @n: <int> number of characters to concat
+ * #src: source <char> array
+ * @dest: destination <char> array
+ *
+ * Return: pointer to `dest`
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int cnt = 0;
+	char *res = dest;
+
+	while (*dest)
+		dest++;
+
+	while (cnt++ < n && *src)
+		*dest++ = *src++;
+
+	return (res);
+}
