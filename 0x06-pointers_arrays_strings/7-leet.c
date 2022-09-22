efb14a59c3fb78624e48da97ef1d2c3c95913fd2
tumerick <magukwert@gmail.com>
2022-09-23 01:42:14 +0200

    commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - converts a string into "leet" speak.
+ *
+ * Return: 1337ed string.
+ */
+
+char *leet(char *s)
+{
+	int i;
+	char table[] = {'4', '3', '1', '0', '7'};
+	char upper[] = {'A', 'E', 'I', 'O', 'T'};
+	char lower[] = {'a', 'e', 'i', 'o', 't'};
+
+	while (*s != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == upper[i] || *s == lower[i])
+				*s = table[i];
+		}
+		s++;
+	}
+
+	return (s);
+}
