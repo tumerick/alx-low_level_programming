f88e83f3cc83d6ffac738cfe4d022e7c11cef218
tumerick <magukwert@gmail.com>
2022-09-22 16:41:23 +0200

    commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * s1: <char> array
+ * s2: <char> array
+ *
+ * Return: <int>
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && (*s1++ == *s2++))
+		;
+	return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+		
+}
