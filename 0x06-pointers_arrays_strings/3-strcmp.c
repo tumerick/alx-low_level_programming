3037db4e8b4a7cff8fbbf1965b7961c57a29f562
tumerick <magukwert@gmail.com>
2022-09-22 16:44:51 +0200

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,8 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	};
+	while (*s1 && (*s1++ == *s2++))
+		;
 
 	return (*(const unsigned char *) s1 - *(const unsigned char *) s2);
 }
