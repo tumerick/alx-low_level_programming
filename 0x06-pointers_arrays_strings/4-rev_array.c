70ee669bfb048e9532f972c9c08eea4bf3218178
tumerick <magukwert@gmail.com>
2022-09-22 23:48:00 +0200

    commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the elements of 
+ * an array of integers
+ *
+ * @a: <int> array
+ * @n: <int> num of elements to swap.
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, j = n - 1, tmp;
+
+	for (; i < j; i++, j--)
+	{
+		tmp = *(a + i);
+		*(a + i) = *(a + j);
+		*(a + j) = tmp;
+	}
+}
