c18a310cc414be92d73bfa6d145c757ce83e2ca5
tumerick <magukwert@gmail.com>
2022-09-23 02:19:38 +0200

    commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ *
+ * @s: string to be encoded
+ *
+ * Return: string
+ */
+
+char *rot13(char *s)
+{
+	int i = 0, j;
+	char letters[] = {'A', 'B', 'C', 'D', 'E', 'F',
+		          'G', 'H', 'I', 'J', 'K', 'L',
+		          'M', 'N', 'O', 'P', 'Q', 'R',
+		          'S', 'T', 'U', 'V', 'W', 'X',
+		          'Y', 'Z', 'a', 'b', 'c', 'd',
+		          'e', 'f', 'g', 'h', 'i', 'j',
+		          'k', 'l', 'm', 'n', 'o', 'p',
+		          'q', 'r', 's', 't', 'u', 'v',
+		          'w', 'x', 'y', 'z'};
+	
+	char key[] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		      'T', 'U', 'V', 'W', 'X', 'Y',
+		      'Z', 'A', 'B', 'C', 'D', 'E',
+		      'F', 'G', 'H', 'I', 'J', 'K',
+		      'L', 'M', 'n', 'o', 'p', 'q',
+		      'r', 's', 't', 'u', 'v', 'w',
+		      'x', 'y', 'z', 'a', 'b', 'c',
+		      'd', 'e', 'f', 'g', 'h', 'i',
+		      'j', 'k', 'l', 'm'};
+
+	while (s[i++])
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = key[j];
+				break;
+			}
+		}
+
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *str);
 char *cap_string(char *s);
 char *leet(char *s);
+char *rot13(char *s);
 
 int _strcmp(char *s1, char *s2);
 int _putchar(char c);
