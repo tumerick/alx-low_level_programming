c860055e3149672a0e618b7cd6d91c094c6e4943
tumerick <magukwert@gmail.com>
2022-09-26 17:41:31 +0200

    commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,29 +1,6 @@
 #include "main.h"
 
 /**
- * compare - returns true if `a` and `b` are the same
- *
- * @a: first string
- * @b: second string
- *
- * Return: true || false
- */
-
-int compare(char *a, char *b)
-{
-	while (*a && *b)
-	{
-		if (*a != *b)
-			return (0);
-
-		a++;
-		b++;
-	}
-
-	return (1);
-}
-
-/**
 * _strstr - finds the occurence of a substr `needle`
 * in the string `haystack` the terminating null bytes (\0)
 * are not compared.
@@ -35,12 +12,21 @@ int compare(char *a, char *b)
 
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack != '\0')
-	{
-		if ((*haystack == *needle) &&
-		     compare(haystack, needle))
-			return (haystack);
+	int i;
 
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			while (haystack[i] == needle[i])
+			{
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			}
+		}
 		haystack++;
 	}
 
