d3a2ecf115e098ad5bf64757dca89592bc344258
tumerick <magukwert@gmail.com>
2022-09-26 15:40:31 +0200

    commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies `n` bytes from memory area `src`
+ * to memory area `dest`
+ * 
+ * @src: src memory area
+ * @dest: destination memory area
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (n--)
+		*dest++ = *src++;
+
+	return (dest);
+}
