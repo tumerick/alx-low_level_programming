c11b8896c4596a1a4c9c2c8fdf82969aa7071a94
tumerick <magukwert@gmail.com>
2022-09-28 15:04:23 +0200

    commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - recursively finds the value of `x` raised 
+ * to the power `y`
+ *
+ * @x: base number
+ * @y: exponent
+ *
+ * Return: <int>
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * pow_recursion(x, y - 1);
+}
