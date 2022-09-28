3b34c59e98bb0b7939f497ac32a275f2bf8b3662
tumerick <magukwert@gmail.com>
2022-09-28 15:20:40 +0200

    commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _sqrt - helper function for _sqrt_recursion
+ *
+ * @n: radicand
+ * @i: index
+ *
+ * Return: <int>
+ */
+
+int _sqrt(int n, int i)
+{
+	int root = i * i;
+
+	if (root == num)
+		return (root);
+
+	if (root == num / 2)
+		return (-1);
+
+	return (square(num, root + 1));
+}
+
+/**
+ * _sqrt_recursion - recursively determines the square root of
+ * a given number of type <int>
+ *
+ * @n: <int>
+ *
+ * Return: <int>
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
