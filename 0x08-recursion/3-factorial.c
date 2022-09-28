08e49e8a75919bfe77732f98b3125f41210e60b0
tumerick <magukwert@gmail.com>
2022-09-28 14:32:59 +0200

    commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - determines the factorial of a given integer
+ *
+ * @n: <int>
+ *
+ * Return: <int>
+ */
+
+int factorial(int n)
+{
+	if (n <= 0)
+		return (-1);
+
+	return (n * factorial(n - 1));
+}
