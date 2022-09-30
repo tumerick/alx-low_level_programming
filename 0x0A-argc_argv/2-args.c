ccb35650ce402f1c3e006ab510f4ba0dce50047b
tumerick <magukwert@gmail.com>
2022-09-30 15:22:45 +0200

    commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints recieved arguments
+ *
+ * @argc: number of CL arguments
+ * @argv: array of CL arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	
+	for (; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
