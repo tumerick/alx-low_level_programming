475635fef7526ea546e01b7adf8d2bfde498eda3
tumerick <magukwert@gmail.com>
2022-09-30 14:52:00 +0200

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** 
+ * main - program that prints it's 'own' name
+ *
+ * Return: Always 0
+ */
+
+int main(void argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
