5c66fc0d6a34124271d973ab16295f2cd4015c20
tumerick <magukwert@gmail.com>
2022-09-30 15:11:38 +0200

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: Always 0
+ */
+
+int main(void argc, char *argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
