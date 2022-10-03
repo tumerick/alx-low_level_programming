c2d629eeb72bf79d18fc56e0bab85ccedd6cc203
tumerick <magukwert@gmail.com>
2022-10-03 14:55:27 +0200

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -38,10 +38,11 @@ int **alloc_grid(int width, int height)
 			free(result);
 			return (NULL);
 		}
+	}
 
+	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			result[i][j] = 0;
-	}
 
 	return (result);
 }
