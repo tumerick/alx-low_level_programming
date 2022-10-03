b623ea673dcada1cce49ff35b8b634b3d3ff3ea1
tumerick <magukwert@gmail.com>
2022-10-03 14:19:40 +0200

    commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,14 +16,14 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int len = 0, idx;
 
-	while (s1[len] || s2[len])
-		len++;
-
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+
+	while (s1[len] || s2[len])
+		len++;
 
 	str = (char *) malloc(sizeof(char) * len);
 
