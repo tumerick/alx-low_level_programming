7a0f1a4eba8f335501a6b6873b8acbc8b3f37adf
tumerick <magukwert@gmail.com>
2022-10-03 14:49:36 +0200

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	int **result;
 	int i, j;
 
-	if (wdith == 0 || height == 0)
+	if (width == 0 || height == 0)
 		return (NULL);
 
 	result = (int **) malloc(sizeof(int) * height);
