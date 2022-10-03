af2c81484c4277207b3e811931d84748c0646de3
tumerick <magukwert@gmail.com>
2022-10-03 16:17:50 +0200

    commit

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,7 +5,7 @@ char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 char *argtostr(int ac, char **av);
-char *strtow(char *str);
+char **strtow(char *str);
 
 int **alloc_grid(int height, int width);
 
