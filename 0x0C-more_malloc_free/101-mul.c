4340be46c1e4346d484b211eac01be69fc01a8af
tumerick <magukwert@gmail.com>
2022-10-08 02:38:31 +0200

    commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,94 +2,78 @@
 #include <stdlib.h>
 
 /**
- * _print - prints a string followed by a new line
- *
- * @str: pointer to <string>
- */
-
-void _print(char *str)
+ * print - prints a string followed by a new line,
+ * @str: <str>
+ * Return: void
+*/
+void print(char *str)
 {
 	while (*str++)
 		_putchar(*str);
 }
 
 /**
- * _atoi - converts a string to an integer
- *
- * @s: <string>
- *
- * Return: <int>
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
  */
 
-int _atoi(char *s)
+int _atoi(const char *s)
 {
 	int sign = 1;
-	unsigned long int res = 0, num1, idx;
+	unsigned long int resp = 0, num, idx;
 
-	for (num1 = 0; !(s[num1] >= 48) && !(s[num1] <= 57); num1++)
-	{
-		if (s[num1] == '-')
-		{
+	for (num = 0; !(s[num] >= 48 && s[num] <= 57); num++)
+		if (s[num] == '-')
 			sign *= -1;
-		}
-	}
 
-	for (idx = num1; s[idx] >= 48 && s[idx] <= 57; idx++)
+	for (idx = num; s[idx] >= 48 && s[idx] <= 57; idx++)
 	{
-		res *= 10;
-		res += (s[idx] - 48);
+		resp *= 10;
+		resp += s[idx] - 48;
 	}
 
-	return (sign * res);
+	return (sign * resp);
 }
 
 /**
- * print_int - prints an integer
- *
+ * print_int - prints an integer.
  * @n: int
+ * Return: 0
  */
 
 void print_int(unsigned long int n)
 {
-	unsigned long int div = 1, idx, res;
+	unsigned  long int div = 1, idx;
 
 	for (idx = 0; n / div > 9; idx++, div *= 10)
-	;
-
+		;
 	for (; div >= 1; n %= div, div /= 10)
-	{
-		res = n / div;
-		_putchar('0' + res);
-	}
+		_putchar('0' + (n / div));
 }
 
 /**
- * main - multiplies two recieved <int> values and prints the result
- *
- * @argc: <int>
- * @argv: <array>
- *
+ * main - multiplies two numbers together, recieved from argv
+ * @argc: int
+ * @argv: list
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
-	int multiplicand, multiplier;
-
-	(void) argc;
+	int x, y;
+	(void)argc;
 
 	if (argc < 3)
 	{
-		_print("Error");
+		print("Error ");
 		exit(98);
 	}
 
-	multiplicand = _atoi(argv[1]);
-	multiplier = _atoi(argv[2]);
-
-	print_int(multiplicand * multiplier);
+	x = _atoi(argv[1]);
+	y = _atoi(argv[2]);
+	print_int(x * y);
 
 	_putchar('\n');
-
 	return (0);
 }
