872d44f72c50c13e0ef5d6fe65480b7b1e00a8c7
tumerick <magukwert@gmail.com>
2022-10-10 17:03:10 +0200

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,17 +13,17 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		printf("Name: %s\n", "(nil)");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
 	if (d->age < 0)
-		printf("Age: %s\n", "(nil)");
+		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("Owner: %s\n", "(nil)");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner %s\n", d->owner);
 }
