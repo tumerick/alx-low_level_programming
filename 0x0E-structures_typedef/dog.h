b8ffd44377532eb57b1c69149db3e7554ae072a2
tumerick <magukwert@gmail.com>
2022-10-10 14:31:02 +0200

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a new struct `dog`
+ *
+ * @name: name of the dog <string>
+ * @age: age of the dog <float>
+ * @owner: owner of the dog <string>
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
+
+#endif
