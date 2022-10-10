c13b47d3d08cfba740b569f9717c1f26539726e1
tumerick <magukwert@gmail.com>
2022-10-10 18:17:56 +0200

    commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees the memory allocated to a `dog` type
+ *
+ * @d: reference to allocated memory to be freed.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d);
+}
