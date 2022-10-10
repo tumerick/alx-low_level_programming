04efe12920b76af00ab097653deec977ca0182f4
tumerick <magukwert@gmail.com>
2022-10-10 14:38:25 +0200

    commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type `dog`
+ *
+ * @d: <dog>
+ * @name: name of the dog <string>
+ * @age: age of the dog <float>
+ * @owner: owner of the dog <string>
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
