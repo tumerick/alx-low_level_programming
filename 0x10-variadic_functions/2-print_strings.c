df4b7e3179607be810dfca1fff93d1a20ac9387c
tumerick <magukwert@gmail.com>
2022-10-14 16:59:11 +0200

    commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ *
+ * @separator: <string> seperator
+ * @n: <int> num of strings passed to function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	char *str;
+	va_list params;
+	const unsigned int i = 0;
+
+	va_start(params, n);
+	for (; i < n; i++)
+	{
+		str = va_arg(params, char *);
+
+		if (!(str))
+			printf("(nil)");
+
+		else
+			printf("%s", str);
+
+		if (i != (n - 1) && separator)
+			printf("%s", separator);
+	}
+	va_end(params);
+
+	printf("\n");
+}
