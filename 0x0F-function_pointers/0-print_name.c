c3fb88a026fb4ddb7f09c1e34fd2fd5ddfafa304
tumerick <magukwert@gmail.com>
2022-10-14 17:36:37 +0200

    commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ *
+ * @name: <string>
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*s)(char *))
+{
+	if (!name || !f)
+		return;
+
+	f(name);
+}
