a3d0bef05a11c4c02f711785269f7bdc49c61d7a
tumerick <magukwert@gmail.com>
2022-10-21 16:11:57 +0200

    commit

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -32,7 +32,10 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->next = NULL;
 
 	if (!*head)
-		return (new_node);
+	{
+		*head = new_node;
+	}
+		return (*head);
 
 	last = *head;
 	while (last->next != NULL)
