86eb8843e155f3c3841ca56abb5a400bb14f7d4b
tumerick <magukwert@gmail.com>
2022-10-21 16:52:34 +0200

    commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,48 +3,23 @@
 ; Auth: Joshua Burger
 ; Desc: 64-bit assembly program that prints "Hello, Holverton\n"
 
-	.file	"tmp.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
+extern printf
+
+section .text
+	global main
+
 main:
-.LFB0:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
+	pushq	rbp
+
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+
+	pop rbp
+	
+	mov rax,0
 	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
+
+section .data
+	msg: db "Hello, Holberton",0
+	fmt: db "%s",10,0
