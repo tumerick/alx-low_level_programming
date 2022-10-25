ab79f11dd67f9dc0e69713e2ac4141d87d7e0be7
tumerick <magukwert@gmail.com>
2022-10-25 17:23:38 +0200

    commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,9 +9,9 @@
  * Return: NULL (Fail) || addres of the new element
  */
 
-listint_t add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *new_node, *tmp = *head;
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
@@ -26,10 +26,10 @@ listint_t add_nodeint_end(listint_t **head, const int n)
 		return (new_node);
 	}
 
-	while (*h)
-		*h = *h->next;
+	while (tmp->next)
+		tmp = tmp->next;
 
-	h->next = next;
+	tmp->next = new_node;
 
 	return (new_node);
 }
