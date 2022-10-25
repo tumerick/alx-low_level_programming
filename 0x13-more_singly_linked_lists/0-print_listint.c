778c497183b78b84d2c3131d00aea6e3ffe3d89a
tumerick <magukwert@gmail.com>
2022-10-25 16:53:42 +0200

    commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t 	num_nodes = 0;
+	size_t num_nodes = 0;
 
 	while (h)
 	{
-		++num_nodes;
 		printf("%d\n", h->n);
 		h = h->next;
+		++num_nodes;
 	}
 
 	return (num_nodes);
